5c189ae59df45bd86d7259458f50f27363234c8d
kostasdrakonakis <kostasdrakonakis13@gmail.com>
2020-03-14 15:55:01 +0100

    Add @Documented annotation in annotations

--- randomizer-annotations/src/main/java/com/kostasdrakonakis/randomizer/annotations/RandomFloat.java
+++ randomizer-annotations/src/main/java/com/kostasdrakonakis/randomizer/annotations/RandomFloat.java
@@ -1,5 +1,6 @@
 package com.kostasdrakonakis.randomizer.annotations;
 
+import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -7,6 +8,7 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.CLASS)
 @Target(value = ElementType.FIELD)
+@Documented
 public @interface RandomFloat {
     float minValue() default 0F;
     float maxValue() default 10000F;
